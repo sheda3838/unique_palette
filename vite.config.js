0fbb82f70cdfc6e6614467132c8afc124d2b518b
Zaid Kamil <kamilzaid53@gmail.com>
2026-01-18 23:45:56 +0530

    Changes vite config added base

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,8 @@ export default defineConfig({
         laravel({
             input: ['resources/css/app.css', 'resources/js/app.js'],
             refresh: true,
+            buildDirectory: 'public/build',
         }),
     ],
+    base: '/', // Important for correct paths in production
 });
