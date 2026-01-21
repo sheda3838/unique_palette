08cd166390831e1790b5490b914f59a7a8d1c2fd
Zaid Kamil <kamilzaid53@gmail.com>
2026-01-21 18:55:57 +0530

    Artist my artworks page url changed again

--- resources/views/livewire/artist/artworks.blade.php
+++ resources/views/livewire/artist/artworks.blade.php
@@ -42,7 +42,7 @@
                 </div>
 
                 <div class="relative h-72 overflow-hidden">
-                    <img src="{{ asset('assets/' . $artwork->image_path) }}" alt="{{ $artwork->title }}" class="w-full h-full object-cover group-hover:scale-110 transition-transform duration-700">
+<img src="{{ asset('' . $artwork->image_path) }}" alt="{{ $artwork->title }}" class="w-full h-full object-cover group-hover:scale-110 transition-transform duration-700">
                     <div class="absolute inset-0 bg-black/5 group-hover:bg-black/20 transition-colors"></div>
                 </div>
 
@@ -116,7 +116,7 @@
                     <div class="lg:flex lg:items-start lg:gap-10">
                         <div class="lg:w-1/2">
                             <div class="rounded-[30px] overflow-hidden shadow-inner bg-gray-50 border border-gray-100 relative">
-                                <img src="{{ asset('assets/' . $selectedArtwork->image_path) }}" class="w-full h-auto object-cover max-h-[500px]" alt="{{ $selectedArtwork->title }}">
+<img src="{{ asset('' . $selectedArtwork->image_path) }}" class="w-full h-auto object-cover max-h-[500px]" alt="{{ $selectedArtwork->title }}">
                                 {{-- Modal Status Badge --}}
                                 <div class="absolute top-6 right-6">
                                     <span class="px-4 py-2 text-[10px] font-black rounded-full shadow-lg uppercase tracking-widest border-2 bg-white/90 text-[#2C3E50]">
