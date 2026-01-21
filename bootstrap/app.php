3f9371f8c5094bd5aa0ef72ffe426f39066fb397
Zaid Kamil <kamilzaid53@gmail.com>
2026-01-21 19:31:05 +0530

    Added trsut proxies file

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -12,6 +12,10 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
+        $middleware->use([
+            \App\Http\Middleware\TrustProxies::class,
+        ]);
+
         $middleware->web(append: [
             \App\Http\Middleware\SetLocale::class,
         ]);
