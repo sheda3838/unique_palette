d38c23455a6c1177dedbdfe11cb6127b456f5ad5
Zaid Kamil <kamilzaid53@gmail.com>
2026-01-26 14:32:41 +0530

    added a default profile picture

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,7 +2,6 @@
 
 namespace App\Models;
 
-use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
@@ -119,7 +118,7 @@ class User extends Authenticatable
             return asset('storage/' . $this->profile_photo_path);
         }
 
-        return asset('assets/default-avatar.png');
+        return asset('assets/default_profile.png');
     }
 
     public function getHasProfileImageBlobAttribute(): bool
